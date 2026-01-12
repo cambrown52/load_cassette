7b03f02dd6dcb452b3d5915d383f961c87756362
Cameron Brown <cab@goldwinddenmark.com>
2026-01-12 10:40:37 +0100

    add Idling state

---
+++ Matlab/+cassette/+simulation/+turbinestate/Idling.m
@@ -0,0 +1,31 @@
+classdef Idling < cassette.simulation.turbinestate.BaseState
+    %OPERATION Summary of this class goes here
+    %   Detailed explanation goes here
+
+    properties
+
+    end
+
+    methods
+        function obj = Idling(yaw)
+            obj@cassette.simulation.turbinestate.BaseState(yaw)
+        end
+    end
+    methods
+
+        function to_bladed(obj,template)
+            arguments
+                obj
+                template cassette.templates.BladedTemplate
+            end
+            % set turbine state to operational in both places in IN file
+            template.replaceProperty("CALCN",14)
+
+            index_runconfig=template.findLine("<RunConfiguration>",method="contains");
+            template.replaceXMLProperty("Calculation",14,after_index=index_runconfig)
+
+            % set yaw error
+            template.replaceProperty("INIMD",obj.yaw*pi/180)
+        end
+    end
+end
