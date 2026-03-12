3e043818ac303ecbf502fe95a8b9246db83f25fe
Cameron Brown <cab@goldwinddenmark.com>
2026-03-12 16:52:36 +0100

    revert accidental deletion

--- Matlab/+cassette/+simulation/+current/ExponentialCurrentProfile.m
+++ Matlab/+cassette/+simulation/+current/ExponentialCurrentProfile.m
@@ -21,7 +21,10 @@ classdef ExponentialCurrentProfile < cassette.simulation.current.BaseCurrent
             CURRENT.ICURRS=1;
             CURRENT.US0Z0=obj.speed;
             CURRENT.MUCS=obj.direction*pi/180;
-            CURRENT.
+            if abs(obj.exponent/(1/7)-1)>100*eps
+                CURRENT.USERSHEAR=-1;
+                CURRENT.CSHEAR=obj.exponent;
+            end
             template.CURRENT=CURRENT;
         end
     end
